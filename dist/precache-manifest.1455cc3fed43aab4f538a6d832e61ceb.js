d7a472f7bd77ee955f34d63668d8883e5b20befc
elrumo <elrumo97@me.com>
2019-11-23 18:27:32 +0000

    mobile app

---
+++ dist/precache-manifest.1455cc3fed43aab4f538a6d832e61ceb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "a3e5723ad7e29760ec5a",
+    "url": "/js/chunk-vendors.41bb8290.js"
+  },
+  {
+    "revision": "799c082da9b64a15a486",
+    "url": "/js/app.55af98f0.js"
+  },
+  {
+    "revision": "33d1b659ed0abe61e9dcf93a508488b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "799c082da9b64a15a486",
+    "url": "/css/app.ccff8ec5.css"
+  }
+];
